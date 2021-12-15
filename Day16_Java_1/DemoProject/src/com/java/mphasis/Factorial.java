5799ba5a520f67b448d75e9f07ee396b7db9f217
Prasanna <prassup.dotnet@gmail.com>
2021-12-15 12:35:53 +0530

    code

---
+++ Day16_Java_1/DemoProject/src/com/java/mphasis/Factorial.java
@@ -0,0 +1,18 @@
+package com.java.mphasis;
+
+public class Factorial {
+
+	public void calc(int n) {
+		int f=1,i=1;
+		while(i <= n) {
+			f=f*i;
+			i++;
+		}
+		System.out.println("Factorial Value " +f);
+	}
+	public static void main(String[] args) {
+		int n=8;
+		Factorial obj = new Factorial();
+		obj.calc(n);
+	}
+}
---
+++ Day16_Java_1/DemoProject/src/com/java/mphasis/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.java.mphasis;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java Programming...");
+	}
+}
