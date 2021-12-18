fb70022ab8c9febde924c9278f3e70ec56061f60
Prasanna <prassup.dotnet@gmail.com>
2021-12-18 12:48:32 +0530

    code

---
+++ Day19_Java_Project/StudentProject/src/com/java/project/StudentException.java
@@ -0,0 +1,12 @@
+package com.java.project;
+
+public class StudentException extends Exception {
+
+	public StudentException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public StudentException(String error) {
+		super(error);
+	}
+}
