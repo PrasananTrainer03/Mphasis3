b5d6ebe908bb425a50e8622f0e3d20bb095ace65
Prasanna <prassup.dotnet@gmail.com>
2021-12-21 10:37:14 +0530

    code

---
+++ Day21_Project_Solution/LeaveDetailsTask/src/com/java/mphasis/LeaveDetailsException.java
@@ -0,0 +1,12 @@
+package com.java.mphasis;
+
+public class LeaveDetailsException extends Exception {
+
+	LeaveDetailsException() {
+		
+	}
+	
+	LeaveDetailsException(String error) {
+		super(error);
+	}
+}
