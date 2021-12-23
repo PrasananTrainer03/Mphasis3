af92e6d09b00135cf87647ae0daaf0db8d7d888e
Prasanna <prassup.dotnet@gmail.com>
2021-12-23 10:11:46 +0530

    code

---
+++ Day22_Tables/JdbcExampleNew/src/main/java/com/java/jdbc/EmployDelete.java
@@ -0,0 +1,35 @@
+package com.java.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class EmployDelete {
+
+	public static void main(String[] args) {
+		int empno;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Employ No   ");
+		empno=sc.nextInt();
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", 
+					"training", "training");
+            String cmd = "Delete from EmployNew where empno=?";
+            PreparedStatement pst = con.prepareStatement(cmd);
+            pst.setInt(1, empno);
+            pst.executeUpdate();
+            System.out.println("*** Record Deleted ***");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+}
